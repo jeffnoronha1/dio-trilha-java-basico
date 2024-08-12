c5923f7b3dc34af93f09c4f57776a4793b33520d
Jefferson Noronha <jefferson.e.n@hotmail.com>
2024-08-12 16:48:33 -0300

    Concluido exercício

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner entradaDeDados = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta ");
+        int numero = entradaDeDados.nextInt();
+        entradaDeDados.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = entradaDeDados.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = entradaDeDados.nextLine();
+
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = entradaDeDados.nextDouble();
+
+        entradaDeDados.close();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + ", conta " + numero + " e seu saldo " +
+                String.format("%.2f", saldo) + " já está disponível para saque.";
+
+        System.out.println(mensagem);
     }
 }
